b788eeba2b2f771a20eaf45b28035cdd6fb03ee6
Ezio <assassinste@gmail.com>
2020-04-22 20:07:37 +0800

    Now you can 'add' new cheeses!

--- everycheese/everycheese/cheeses/models.py
+++ everycheese/everycheese/cheeses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from autoslug.fields import AutoSlugField
@@ -37,3 +38,6 @@ class Cheese(TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("cheeses:detail", kwargs={"slug": self.slug})
