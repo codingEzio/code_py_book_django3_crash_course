75a7948e734ab4f4a18dce1d8f2b95e4f6ee79c1
Ezio <assassinste@gmail.com>
2020-04-22 08:17:19 +0800

    Adding new model 'Cheese' to app 'cheese'

--- everycheese/everycheese/cheeses/models.py
+++ everycheese/everycheese/cheeses/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from autoslug.fields import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+
+class Cheese(TimeStampedModel):
+    """
+    Both of the third-party libraries were used to simplify the common jobs.
+    """
+
+    class Firmness(models.TextChoices):
+        UNSPECIFIED = "unspecified", _("Unspecified")
+        SOFT = "soft", _("Soft")
+        SEMI_SOFT = "semi-soft", _("Semi-Soft")
+        SEMI_HARD = "semi-hard", _("Semi-Hard")
+        HARD = "hard", _("Hard")
+
+    name = models.CharField(_("Name of cheese"), max_length=255)
+    slug = AutoSlugField(
+        _("Cheese Address"),
+        unique=True,
+        always_update=False,
+        populate_from="name",
+    )
+    description = models.TextField(_("Description"), blank=True)
+    firmness = models.CharField(
+        _("Firmness"),
+        max_length=20,
+        choices=Firmness.choices,
+        default=Firmness.UNSPECIFIED,
+    )
+
+    def __str__(self):
+        return self.name
